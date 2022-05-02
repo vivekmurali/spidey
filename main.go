7bddac70b4c6bb4270fd7cf9b34fc03e7a157d74
Vivek Murali <vivekmurali2k@gmail.com>
2022-05-03 03:12:49 +0530

    Ready to release v0.1.0

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"log"
-	"os"
-	"runtime/pprof"
-
 	"github.com/vivekmurali/spidey/cmd"
 )
 
 func main() {
-	f, err := os.Create("prof/cpu.prof")
-	if err != nil {
-		log.Fatalf("Error creating file: %v", err)
-	}
-	pprof.StartCPUProfile(f)
-	defer pprof.StopCPUProfile()
 	cmd.Execute()
 }
